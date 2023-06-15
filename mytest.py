7e575d31383384dc9c916215b02bd61b870abcab
jyaacoub <jyaacoub21@gmail.com>
2023-06-14 20:36:48 -0400

    replicated paper results

--- mytest.py
+++ mytest.py
@@ -3,9 +3,11 @@ import torch
 from torchsummary import summary
 
 data = 'davis'
-path = f'/cluster/home/t122995uhn/projects/DGraphDTA/models/model_GNNNet_{data}.model'
+path = f'models/model_GNNNet_{data}.model'
 m = torch.load(path, map_location=torch.device('cpu'))
 # %%
 print(m)
 summary(m)
 # %%
+for key in list(m.keys()):
+    print(key)
