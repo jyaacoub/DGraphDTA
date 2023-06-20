08a8136a45be1a84a9f5391eb1a87833bb7731a5
jyaacoub <jyaacoub21@gmail.com>
2023-06-20 13:37:01 -0400

    feature extraction optimizations

--- data_process.py
+++ data_process.py
@@ -86,17 +86,26 @@ def residue_features(residue):
 
 # mol atom feature for mol graph
 def atom_features(atom):
-    # 44 +11 +11 +11 +1
-    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),
-                                          ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As',
-                                           'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se',
-                                           'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr',
-                                           'Pt', 'Hg', 'Pb', 'X']) +
-                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    [atom.GetIsAromatic()])
+    # 44 +11 +11 +11 +1 = 78
+    return np.concatenate(
+        (one_hot(atom.GetSymbol(),
+                ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As',
+                'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se',
+                'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr',
+                'Pt', 'Hg', 'Pb', 'X'],                                           cap=True),
+        one_hot(atom.GetDegree(),         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]          ), # WARNING: why use one hot here instead of just the number?
+        one_hot(atom.GetTotalNumHs(),     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], cap=True),
+        one_hot(atom.GetImplicitValence(),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], cap=True),
+        [atom.GetIsAromatic()]))
 
+def one_hot(x, allowable_set, cap=False):
+    """Return the one-hot encoding of x as a numpy array."""
+    if x not in allowable_set:
+        if not cap:
+            raise Exception('input {0} not in allowable set{1}:'.format(x, allowable_set))
+        else:
+            x = allowable_set[-1]
+    return np.eye(len(allowable_set))[allowable_set.index(x)]
 
 # one ont encoding
 def one_of_k_encoding(x, allowable_set):
@@ -116,27 +125,27 @@ def one_of_k_encoding_unk(x, allowable_set):
 # mol smile to mol graph edge index
 def smile_to_graph(smile):
     mol = Chem.MolFromSmiles(smile)
-
     c_size = mol.GetNumAtoms()
 
-    features = []
-    for atom in mol.GetAtoms():
-        feature = atom_features(atom)
-        features.append(feature / sum(feature))
+    # getting node features
+    atoms = mol.GetAtoms()
+    features = np.zeros((len(atoms), 78))
+    for i, atom in enumerate(atoms):
+        feature = atom_features(atom) # 78 features
+        features[i] = feature / sum(feature) # why / sum(feature)? #WARNING: this doesnt make sense
 
-    edges = []
-    for bond in mol.GetBonds():
-        edges.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
+    # getting bonds using rdKit
+    edges = [[b.GetBeginAtomIdx(), b.GetEndAtomIdx()] for b in mol.GetBonds()]
+    
+    # to_directed() is important because the model is trained on directed graph
     g = nx.Graph(edges).to_directed()
-    edge_index = []
-    mol_adj = np.zeros((c_size, c_size))
-    for e1, e2 in g.edges:
-        mol_adj[e1, e2] = 1
-        # edge_index.append([e1, e2])
-    mol_adj += np.matrix(np.eye(mol_adj.shape[0]))
+    mol_adj = nx.to_numpy_array(g) # adjacency matrix
+
+    # adding self loop
+    mol_adj += np.eye(mol_adj.shape[0])
+    # converting edge matrix to edge index for pytorch geometric
     index_row, index_col = np.where(mol_adj >= 0.5)
-    for i, j in zip(index_row, index_col):
-        edge_index.append([i, j])
+    edge_index = np.array([[i,j] for i,j in zip(index_row, index_col)])
     # print('smile_to_graph')
     # print(np.array(features).shape)
     return c_size, features, edge_index
@@ -289,8 +298,7 @@ def create_dataset_for_test(dataset):
     # create smile graph
     smile_graph = {}
     for smile in compound_iso_smiles:
-        g = smile_to_graph(smile)
-        smile_graph[smile] = g
+        smile_graph[smile] = smile_to_graph(smile)
     # print(smile_graph['CN1CCN(C(=O)c2cc3cc(Cl)ccc3[nH]2)CC1']) #for test
 
     # create target graph
